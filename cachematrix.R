f43689bc08e289b5bf7555cea0bb4fd10f457a13
DBright1 <Dariusjbright@gmail.com>
2024-07-09 21:59:09 -0700

    My programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#Cache Matrix initialization
+#for some reason, the onl square matrix that is solvable is 2,2
+#previous tests of matrices 3:3, 4:4, 5:5,6:6, were not working with solve
+makeCacheMatrix <- function(x = matrix(2:5,2,2)){
+#create global variable place holder for inverse matrix
+  inv<<-NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  #create a new matrix that will overite the previous matrix and the cached INV
+  setm <- function(y){
+    x <<- y
+    inv <<-NULL
+  }
+  #view the created matrix in x variable
+  getm <- function() x
+  
+  #create and store the inverse of cached matrix x
+  cacheSolve <- function(z){
+    z <- x
+    #Message warning no previous cache was created  
+    if(!is.null(z)) {
+      message("getting cached data")
+    }
+    #store cached invers in clobal variable
+    inv <<- solve(z)
+    #view matrix inverse
+    getinv <- function()inv
+  }
+  
+  #tried to make the methods in list form to call with $ operator but it doesnt
+  #work
+  
+  #A couple things didnt work as i wanted but as far as im concerned, this 
+  #delivers on what I was asked to do when x is passed into cachesolve() 
+  
+  list(setm = setm, getm = getm,getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
